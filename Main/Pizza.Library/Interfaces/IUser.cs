3b16d39524e3b046a08604331f8857c4b781168f
Randall Valentin <randall_valentin@yahoo.com>
2018-06-29 16:54:52 -0400

    First Commit Not a lot of  stuff has been made.

---
+++ Main/Pizza.Library/Interfaces/IUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pizza.Library.Interfaces
+{
+    public interface IUser
+    {
+        string fName { get; set; }
+        string fLastName { get; set; }
+        string Adress { get; set; }
+
+
+
+    }
+}
---
+++ Main/Pizza.Library/Models/Orders.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pizza.Library.Models
+{
+    public class Orders
+    {
+
+
+
+
+    }
+}
