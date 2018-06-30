f372750ae583fbabc42febaf6cae82e6ad849b43
Randall Valentin <randall_valentin@yahoo.com>
2018-06-30 00:34:57 -0400

    Fixed some try and catch. They no output the correct exeptions. For formating numbers.

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -34,82 +34,95 @@ namespace Main
                     m.PrintIngredients();
                     Console.WriteLine();
 
-
-                    int pizzaOption = int.Parse(Console.ReadLine());
-                    int pizzaQty = 0;
-                    int i;
-                    int count = 0;
-
-                    Random r = new Random();//For random integers that would be use to create random ID's.
-                    int rInt = r.Next(1, 100);//With a range of 1 to 100.
-
-
-
-                    if (int.TryParse(pizzaOption.ToString(), out i))
+                    try
                     {
-                        count++;
-                        if (count == 1)
+                        int pizzaOption = int.Parse(Console.ReadLine());
+                        int pizzaQty = 0;
+                        int i;
+                        int count = 0;
+
+                        Random r = new Random();//For random integers that would be use to create random ID's.
+                        int rInt = r.Next(1, 100);//With a range of 1 to 100.
+
+
+
+
+                        if (int.TryParse(pizzaOption.ToString(), out i))
                         {
-                            if (pizzaOption >= 0 && pizzaOption <= 2)
+                            count++;
+                            if (count == 1)
                             {
-                                try
+                                if (pizzaOption >= 0 && pizzaOption <= 2)
                                 {
-
-                                    #region UserInfo
-                                    Console.WriteLine("Lets place a order but first. Fill the require  information about you. ");
-
-                                    Console.WriteLine("Enter your ID: ");
-                                    string ID = Console.ReadLine();
-
-                                    Console.WriteLine("Enter your First Name: ");
-                                    string fName = Console.ReadLine();
-                                    Console.WriteLine("Enter your Last Name");
-                                    string lName = Console.ReadLine();
-                                    Console.WriteLine("Enter your Address: ");
-                                    string adLine = Console.ReadLine();
-                                    Console.WriteLine("Enter your City: ");
-                                    string adCity = Console.ReadLine();
-                                    Console.WriteLine("Enter your State: ");
-                                    string adState = Console.ReadLine();
-                                    Console.WriteLine();
-                                    #endregion
-
-                                    Console.WriteLine("How many " + m.ingredients[pizzaOption].topping.ToString() + " Pizza do you want? ");
-                                    pizzaQty = int.Parse(Console.ReadLine());
-
-                                    if (int.TryParse(pizzaQty.ToString(), out i))
+                                    try
                                     {
-                                        if (pizzaQty <= m.ingredients[pizzaOption].qty)
+
+                                        #region UserInfo
+                                        Console.WriteLine("Lets place a order but first. Fill the require  information about you. ");
+
+                                        Console.WriteLine("Enter your ID: ");
+                                        string ID = Console.ReadLine();
+
+                                        Console.WriteLine("Enter your First Name: ");
+                                        string fName = Console.ReadLine();
+                                        Console.WriteLine("Enter your Last Name");
+                                        string lName = Console.ReadLine();
+                                        Console.WriteLine("Enter your Address: ");
+                                        string adLine = Console.ReadLine();
+                                        Console.WriteLine("Enter your City: ");
+                                        string adCity = Console.ReadLine();
+                                        Console.WriteLine("Enter your State: ");
+                                        string adState = Console.ReadLine();
+                                        Console.WriteLine();
+                                        #endregion
+
+                                        Console.WriteLine("How many " + m.ingredients[pizzaOption].topping.ToString() + " Pizza do you want? ");
+                                        pizzaQty = int.Parse(Console.ReadLine());
+
+                                        if (int.TryParse(pizzaQty.ToString(), out i))
                                         {
-                                            m.OrderPizza(int.Parse(ID), fName, lName, adLine, adCity, adState, pizzaOption, pizzaQty);
+                                            if (pizzaQty <= m.ingredients[pizzaOption].qty)
+                                            {
+                                                m.OrderPizza(int.Parse(ID), fName, lName, adLine, adCity, adState, pizzaOption, pizzaQty);
+
+                                            }
+                                            else
+                                            {
+                                                Console.WriteLine("We are low on stock at the moment. Please try again later. Thanks.");
+                                            }
+
 
                                         }
-                                        else
-                                        {
-                                            Console.WriteLine("We are low on stock at the moment. Please try again later. Thanks.");
-                                        }
-
+                                    }
+                                    catch (FormatException ex)
+                                    {
+                                        Console.WriteLine($"Unexpected error: {ex.Message}");
 
                                     }
-                                }
-                                catch (FormatException ex)
-                                {
-                                    Console.WriteLine($"Unexpected error: {ex.Message}");
+                                    catch (Exception ex)
+                                    {
+                                        Console.WriteLine($"Unexpected error: {ex.Message}");
 
-                                }
-                                catch (Exception ex)
-                                {
-                                    Console.WriteLine($"Unexpected error: {ex.Message}");
+                                    }
 
                                 }
 
                             }
 
-                        }
 
+
+                        }
+                    }
+                    catch (FormatException ex)
+                    {
+                        Console.WriteLine($"Unexpected error: {ex.Message}");
 
                     }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Unexpected error: {ex.Message}");
 
+                    }
 
 
 
